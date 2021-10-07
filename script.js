2a4895ab6aafcdd31e91a539776aabd947e06525
Pierre Caillet <pierrE.caillet@edu.devinci.fr>
2021-10-07 09:25:03 +0200

    🍱 :lipstick: Add img file Update Css

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function Button(){
   document.getElementById("title").classList.toggle("animation")
+}
 
+var img_lightpink = document.getElementById("img_lightpink")
+var img_lightgreen = document.getElementById("img_lightgreen")
+
+function change_img(){
+    img_lightpink.style.display = "block"
+    img_lightgreen.style.display = "none"
+}
+function change_img_back(){
+    img_lightpink.style.display = "none"
+    img_lightgreen.style.display = "block"
 }
